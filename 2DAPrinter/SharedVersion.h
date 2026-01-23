ecc33c236e2f116408c8a6962c4e0a4c3aae488f
Mgamerz <mgamerz@msn.com>
2026-01-22 19:54:24 -0700

    Update version info for Le2 2da printer.

--- 2DAPrinter/SharedVersion.h
+++ 2DAPrinter/SharedVersion.h
@@ -8,14 +8,14 @@
 
 #define DEVELOPER_A "ME3Tweaks"
 #define DEVELOPER_W L"ME3Tweaks"
-#define ASI_NAME_A "SeqActLog Enabler"
-#define ASI_NAME_W L"SeqActLog Enabler"
-#define ASI_NAME_NO_SPACE_A "SeqActLogEnabler"
-#define ASI_NAME_NO_SPACE_W L"SeqActLogEnabler"
+#define ASI_NAME_A "Bio2DA Printer"
+#define ASI_NAME_W L"Bio2DA Printer"
+#define ASI_NAME_NO_SPACE_A "Bio2DAPrinter"
+#define ASI_NAME_NO_SPACE_W L"Bio2DAPrinter"
 
 
-#define ASI_VERSION 5
-#define ASI_DESCRIPTION "SeqActLog Enabler enables functionality for SeqAct_Log kismet objects. It requires an additional mod to be installed for full functionality."
+#define ASI_VERSION 3
+#define ASI_DESCRIPTION "Bio2DA Printer dumps loaded 2DA tables to the log when pressing CTRL + 2."
 
 
 
@@ -23,22 +23,24 @@
 // === Identification from automated ME3Tweaks tooling ====
 #if defined(SDK_TARGET_LE1)
 #define GAME_PREFIX_RC    "LE1"
-#define ASI_GROUP_ID_RC 37
+#define ASI_GROUP_ID_RC 43
 #define ASI_GAME_ID_RC 4
 #endif
 
 #if defined(SDK_TARGET_LE2)
 #define GAME_PREFIX_RC    "LE2"
-#define ASI_GROUP_ID_RC 73
+#define ASI_GROUP_ID_RC 50
 #define ASI_GAME_ID_RC 5
 #endif
 
+// 2DA not used in LE3
+/*
 #if defined(SDK_TARGET_LE3)
 #define GAME_PREFIX_RC    "LE3"
 #define ASI_GROUP_ID_RC 74
 #define ASI_GAME_ID_RC 6
 #endif
-
+*/
 
 // Constants for all ASIs - DO NOT CHANGE BELOW
 // Helper macros for converting version numbers to strings
